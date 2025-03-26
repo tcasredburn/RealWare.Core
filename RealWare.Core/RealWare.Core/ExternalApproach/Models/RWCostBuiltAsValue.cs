f1aa20e0ca9a05ba7c6c840448a661dd4ad130bf
Sean Redburn <sredburn@tulsacounty.org>
2025-03-26 16:12:10 -0500

    Added CostExternalMarketApproachResult and RWCostBuiltAsValue.

---
+++ RealWare.Core/RealWare.Core/ExternalApproach/Models/RWCostBuiltAsValue.cs
@@ -0,0 +1,9 @@
+﻿namespace RealWare.Core.ExternalApproach.Models
+{
+    public class RWCostBuiltAsValue
+    {
+        public double? ImpNo { get; set; }
+        public double? BuiltAsId { get; set; }
+        public double? ExternalCostValue { get; set; }
+    }
+}
---
+++ RealWare.Core/RealWare.Core/ExternalApproach/Models/Result/CostExternalMarketApproachResult.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace RealWare.Core.ExternalApproach.Models.Result
+{
+    /// <summary>
+    /// Result returned from the External Cost Approach service.
+    /// </summary>
+    public class CostExternalMarketApproachResult
+    {
+        public string AccountNo { get; set; }
+        public double? ImpNo { get; set; }
+        public double? TotalExternalCostValue { get; set; }
+        public List<RWCostBuiltAsValue> BuiltAs { get; set; }
+    }
+}
