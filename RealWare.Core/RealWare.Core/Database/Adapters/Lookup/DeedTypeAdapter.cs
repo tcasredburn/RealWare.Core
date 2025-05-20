83f50bda56279c687cdd5e570a6f08176f684159
Sean Redburn <sredburn@tulsacounty.org>
2025-05-20 15:20:22 -0500

    Added DeedTypeAdapter and DeedTypeDto.

---
+++ RealWare.Core/RealWare.Core/Database/Adapters/Lookup/DeedTypeAdapter.cs
@@ -0,0 +1,30 @@
+﻿using RealWare.Core.Database.Adapters.Base;
+using RealWare.Core.Database.Models.Encompass.Lookup;
+using System.Collections.Generic;
+using System.Data;
+
+namespace RealWare.Core.Database.Adapters.Lookup
+{
+    public class DeedTypeAdapter : BaseRealWareDatabaseAdapter, IRealWareDatabaseAdapter
+    {
+        public string TableName => "Encompass.TlkpDeedType";
+
+        public string[] IdentifierColumns => new string[] { "DeedCode", "JurisdictionId" };
+
+        public string[] SortColums => new string[] { "SortOrder" };
+
+        public DeedTypeAdapter(IDbConnection connection) : base(connection) { }
+
+        public List<DeedTypeDto> GetAllActive()
+        {
+            var whereClause = new string[] { "ActiveFlag != 0" };
+            var query = GetDefaultSelectQueryText(this,
+                selectColumns: null,
+                isDistinct: true,
+                whereClause: whereClause,
+                orderBy: SortColums);
+
+            return ExecuteQuery<DeedTypeDto>(query);
+        }
+    }
+}
---
+++ RealWare.Core/RealWare.Core/Database/Models/Encompass/Lookup/DeedTypeDto.cs
@@ -0,0 +1,44 @@
+﻿using RealWare.Core.Database.Models.Encompass.Base;
+using System;
+
+namespace RealWare.Core.Database.Models.Encompass.Lookup
+{
+    public class DeedTypeDto : RealWareDtoBase
+    {
+        public string DeedCode { get; set; }
+        public string DeedDescription { get; set; }
+        public decimal SortOrder { get; set; }
+        public decimal ActiveFlag { get; set; }
+        public decimal EffectExemptionsFlag { get; set; }
+        public decimal EffectCapFlag { get; set; }
+        public decimal IncludeInSalesRatiosFlag { get; set; }
+        public decimal JurisdictionId { get; set; }
+        public decimal NonSaleFlag { get; set; }
+        public DateTime LastUpdated { get; set; }
+
+        /// <summary>
+        /// Returns true if the ActiveFlag is not 0
+        /// </summary>
+        public bool IsActive => ActiveFlag != 0;
+
+        /// <summary>
+        /// Returns true if the EffectExemptionsFlag is not 0
+        /// </summary>
+        public bool IsEffectExemptionsFlag => EffectExemptionsFlag != 0;
+
+        /// <summary>
+        /// Returns true if the EffectCapFlag is not 0
+        /// </summary>
+        public bool IsEffectCapFlag => EffectCapFlag != 0;
+
+        /// <summary>
+        /// Returns true if the IncludeInSalesRatiosFlag is not 0
+        /// </summary>
+        public bool IsIncludeInSalesRatiosFlag => IncludeInSalesRatiosFlag != 0;
+
+        /// <summary>
+        /// Returns true if the NonSaleFlag is not 0
+        /// </summary>
+        public bool IsNonSaleFlag => NonSaleFlag != 0;
+    }
+}
